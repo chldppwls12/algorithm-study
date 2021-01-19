62a8415d7ed3cc66bbb818aa6b8df2637f7ce549
unknown <suker80@naver.com>
2021-01-19 13:35:29 +0900

    Update 1912.py

--- code/Normal-case/2021_01_14/1912.py
+++ code/Normal-case/2021_01_14/1912.py
@@ -1,6 +1,10 @@
+import sys
 n = int(input())
+lst = list(map(int, sys.stdin.readline().split()))
 
-graph = [ list(map(int , input().split())) for _ in range(n)]
+sum_lst = [lst[0]]
 
+for i in range(len(lst)-1):
+    sum_lst.append(max(sum_lst[i]+lst[i+1], lst[i+1]))
 
-print(grpah )
+print(max(sum_lst))
