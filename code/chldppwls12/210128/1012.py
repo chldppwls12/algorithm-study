3cb86abde4f8e5540885a2bd9b6b5c5dca5734ee
chldppwls12 <chldppwls12@naver.com>
2021-01-28 20:39:47 +0900

    210128_9656

--- code/chldppwls12/210128/1012.py
+++ code/chldppwls12/210128/1012.py
@@ -4,13 +4,16 @@ sys.setrecursionlimit(10000)
 def dfs(x, y):
     if x <= -1 or x >= n or y <=-1 or y >= m:
         return False
+    #현재 위치에 배추가 있으면
     if graph[x][y] == 1:
-        graph[x][y] = 0
+        graph[x][y] = 0 #현재 위치의 탐색을 완료했으므로 0으로 바꾸기
         dfs(x - 1, y)
         dfs(x, y-1)
         dfs(x + 1, y)
         dfs(x, y+1)
+        #배추 주변에 있는 인접한 배추들 모두 방문 후 true 반환
         return True
+    #현재 위치에 배추가 없으면 false 반환
     return False
 
 test = int(input())
@@ -24,6 +27,7 @@ for _ in range(test):
     ctn = 0
     for i in range(n):
         for j in range(m):
+            #현재 위치에서 DFS 수행/ 현재 위치가 배추 밀집 지역이면
             if dfs(i, j) == True:
                 ctn = ctn + 1
     result.append(ctn)
--- code/chldppwls12/210128/2579.py
+++ code/chldppwls12/210128/2579.py
@@ -13,5 +13,7 @@ else:
     dp.append(max(l[0]+l[1], l[1]))
     dp.append(max(l[0]+ l[2], l[1]+ l[2]))
     for i in range(3, n):
+        #end-2 계단을 밟은 경우 => dp[i-2] + 마지막 계단
+        #end-1인 경우 end-2 계단을 밟으면 안됨 => dp[i-3] + i-1 계단 + 마지막 계단
         dp.append(max(dp[i-2]+ l[i], dp[i-3]+l[i-1]+l[i]))
     print(dp[-1])
